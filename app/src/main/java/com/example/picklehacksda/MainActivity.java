32dd502575f2333ba63c675b13297ab54be9b795
aaronc13 <adcmyb@mst.edu>
2020-11-14 04:16:51 -0600

    main screen

--- app/src/main/java/com/example/picklehacksda/MainActivity.java
+++ app/src/main/java/com/example/picklehacksda/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private FirebaseAuth mAuth;
+    private boolean hasFocus = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
 
         TextView textView = findViewById(R.id.game_title);
-        textView.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.pulse));
+        //textView.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.pulse));
 
         Button button = findViewById(R.id.quickplay_button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -62,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         Button button2 = findViewById(R.id.multiplayer_button);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,6 +74,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
 
     @Override
     public void onStart() {
