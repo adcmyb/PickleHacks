e1641e3b76b127d504effbd0178b428eea3c0148
RollickBoon <dnshelby712@gmail.com>
2020-11-13 21:43:59 -0600

    firebase

--- app/src/main/java/com/example/picklehacksda/GameplayActivity.java
+++ app/src/main/java/com/example/picklehacksda/GameplayActivity.java
@@ -2,23 +2,31 @@ package com.example.picklehacksda;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.health.SystemHealthManager;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.devbrackets.android.exomedia.BuildConfig;
 import com.devbrackets.android.exomedia.listener.OnPreparedListener;
 import com.devbrackets.android.exomedia.ui.widget.VideoView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.yausername.youtubedl_android.YoutubeDL;
 import com.yausername.youtubedl_android.YoutubeDLException;
 import com.yausername.youtubedl_android.YoutubeDLRequest;
+
+import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -28,7 +36,6 @@ import io.reactivex.schedulers.Schedulers;
 
 public class GameplayActivity extends AppCompatActivity {
 
-    private EditText guess;
     private VideoView videoView;
     private ProgressBar pbLoading;
     private boolean hasFocus = true;
@@ -93,7 +100,6 @@ public class GameplayActivity extends AppCompatActivity {
     private void initViews() {
         videoView = findViewById(R.id.video_view);
         pbLoading = findViewById(R.id.pb_status);
-        guess = findViewById(R.id.guess);
     }
 
     private void initListeners() {
@@ -113,34 +119,56 @@ public class GameplayActivity extends AppCompatActivity {
 
     private void startStream() {
         String url = "https://www.youtube.com/watch?v=dyQMsjU7laE";
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("advertisers")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+//                            int i = rand.nextInt(1000);
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                System.out.println(document.getId() + " => " + document.getData());
+                                List<String> urls = (List<String>) document.get("urls");
+//                                url = urls[0];
+                                pbLoading.setVisibility(View.VISIBLE);
+                                Disposable disposable = Observable.fromCallable(() -> {
+                                    YoutubeDLRequest request = new YoutubeDLRequest(url);
+                                    // best stream containing video+audio
+                                    request.addOption("-f", "best");
+                                    return YoutubeDL.getInstance().getInfo(request);
+                                })
+                                        .subscribeOn(Schedulers.newThread())
+                                        .observeOn(AndroidSchedulers.mainThread())
+                                        .subscribe(streamInfo -> {
+                                            pbLoading.setVisibility(View.GONE);
+                                            String videoUrl = streamInfo.getUrl();
+                                            if (TextUtils.isEmpty(videoUrl)) {
+                                                Toast.makeText(GameplayActivity.this, "failed to get stream url", Toast.LENGTH_LONG).show();
+                                            } else {
+                                                setupVideoView(videoUrl);
+                                            }
+                                        }, e -> {
+                                            if(BuildConfig.DEBUG) Log.e(TAG,  "failed to get stream info", e);
+                                            pbLoading.setVisibility(View.GONE);
+                                            Toast.makeText(GameplayActivity.this, "streaming failed. failed to get stream info", Toast.LENGTH_LONG).show();
+                                        });
+                                compositeDisposable.add(disposable);
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+
 //        String url = etUrl.getText().toString().trim();
         if (TextUtils.isEmpty(url)) {
             return;
         }
 
-        pbLoading.setVisibility(View.VISIBLE);
-        Disposable disposable = Observable.fromCallable(() -> {
-            YoutubeDLRequest request = new YoutubeDLRequest(url);
-            // best stream containing video+audio
-            request.addOption("-f", "best");
-            return YoutubeDL.getInstance().getInfo(request);
-        })
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(streamInfo -> {
-                    pbLoading.setVisibility(View.GONE);
-                    String videoUrl = streamInfo.getUrl();
-                    if (TextUtils.isEmpty(videoUrl)) {
-                        Toast.makeText(GameplayActivity.this, "failed to get stream url", Toast.LENGTH_LONG).show();
-                    } else {
-                        setupVideoView(videoUrl);
-                    }
-                }, e -> {
-                    if(BuildConfig.DEBUG) Log.e(TAG,  "failed to get stream info", e);
-                    pbLoading.setVisibility(View.GONE);
-                    Toast.makeText(GameplayActivity.this, "streaming failed. failed to get stream info", Toast.LENGTH_LONG).show();
-                });
-        compositeDisposable.add(disposable);
+
     }
 
     private void setupVideoView(String videoUrl) {
